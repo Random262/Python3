dedc021980384852049b97a2086e757eed441b8e
Random262 <43027382+Random262@users.noreply.github.com>
2023-12-04 23:55:32 +0500

    Add files via upload

---
+++ buns/mod10/task1.py
@@ -0,0 +1,29 @@
+import requests
+import json
+from collections import OrderedDict
+
+my_req = requests.get('https://swapi.dev/api/starships/10/')
+ship_data = json.loads(my_req.text, object_pairs_hook=OrderedDict)
+ship_data["ship_name"] = ship_data["name"]
+
+lib = ["ship_name", "starship_class", "max_atmosphering_speed", "pilots"]
+falcon = OrderedDict((key, ship_data[key]) for key in lib)
+
+pilot_lib = ["name", "height", "mass", "homeworld", "homeworld_url"]
+pilots = []
+for item in falcon["pilots"]:
+    pilot_req = requests.get(item)
+    pilot_data = json.loads(pilot_req.text, object_pairs_hook=OrderedDict)
+    pilot_data["homeworld_url"] = pilot_data["homeworld"]
+
+    home_req = requests.get(pilot_data["homeworld_url"])
+    home_data = json.loads(home_req.text)
+
+    pilot_data["homeworld"] = home_data["name"]
+    pilots.append(OrderedDict((key, pilot_data[key]) for key in pilot_lib))
+falcon["pilots"] = pilots
+
+with open('falcon.json', 'w') as file:
+    json.dump(falcon, file, indent=4)
+
+print(json.dumps(falcon, indent=4))
